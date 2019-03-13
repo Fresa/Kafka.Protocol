2ba554b42a012111a6e157d985416b48e184ba64
Fredrik Arvidsson <fredrik@fkan.se>
2019-03-13 21:25:36 +0100

    clean up some tests

--- Tests/Kafka.Protocol.Generator.Tests/ExpressionParserTests.cs
+++ Tests/Kafka.Protocol.Generator.Tests/ExpressionParserTests.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
 using Kafka.Protocol.Generator.BackusNaurForm;
 using Kafka.Protocol.Generator.BackusNaurForm.Parsers;
-using Kafka.Protocol.Generator.Definitions;
-using Kafka.Protocol.Generator.Definitions.Parsers;
 using Test.It.With.XUnit;
 using Xunit;
 
@@ -14,10 +11,12 @@ namespace Kafka.Protocol.Generator.Tests
     {
         public class When_parsing_a_expression_with_or : XUnit2Specification
         {
-            private FieldReferenceParser _parser;
-            private SymbolSequence _symbolSequence;
             private Queue<SymbolSequence> _symbolSequences;
             private string _expression;
+
+            public When_parsing_a_expression_with_or()
+            {
+            }
 
             protected override void Given()
             {
@@ -31,147 +30,24 @@ namespace Kafka.Protocol.Generator.Tests
             }
 
             [Fact]
-            public void It_should_have_parsed_the_type_name()
-            {
-                _symbolSequences.Should().HaveCount(5);
-            }
-
-            [Fact]
             public void It_should_have_parsed_the_fields()
             {
-                _symbolSequences.ToList()[0]
-                    .Should().Be(
+                _symbolSequences.ToArray()
+                    .Should()
+                    .HaveCount(5)
+                    .And
+                    .Subject
+                    .Should()
+                    .BeEquivalentTo(
                         SymbolSequence.Operands.Required(
-                            "Size"));
-            }
-
-            [Fact]
-            public void It_should_have_parsed_the_fields_2()
-            {
-                _symbolSequences.ToList()[1]
-                    .Should().Be(
+                            "Size"),
                         SymbolSequence.Operands.Required(
-                            "RequestMessage"));
-            }
-
-            [Fact]
-            public void It_should_have_parsed_the_fields_3()
-            {
-                _symbolSequences.ToList()[2]
-                    .Should().Be(
+                            "RequestMessage"),
                         SymbolSequence.Operands.Required(
-                            "ResponseMessage"));
-            }
-
-            [Fact]
-            public void It_should_have_parsed_the_fields_4()
-            {
-                _symbolSequences.ToList()[3]
-                    .Should().Be(
-                        SymbolSequence.Operators.Or);
-            }
-
-            [Fact]
-            public void It_should_have_parsed_the_fields_5()
-            {
-                _symbolSequences.ToList()[4]
-                    .Should().Be(
+                            "ResponseMessage"),
+                        SymbolSequence.Operators.Or,
                         SymbolSequence.Operators.And);
-
             }
-
-            [Fact]
-            public void Bla()
-            {
-                infixToPostfix("b*(c+d)")
-                    .Should().Be("bcd+*");
-
-            }
-        }
-
-        internal static int Prec(char ch)
-        {
-            switch (ch)
-            {
-                case '+':
-                case '-':
-                    return 1;
-
-                case '*':
-                case '/':
-                    return 2;
-
-                case '^':
-                    return 3;
-            }
-            return -1;
-        }
-
-        // The main method that converts given infix expression  
-        // to postfix expression.   
-        public static string infixToPostfix(string exp)
-        {
-            // initializing empty String for result  
-            string result = "";
-
-            // initializing empty stack  
-            Stack<char> stack = new Stack<char>();
-
-            for (int i = 0; i < exp.Length; ++i)
-            {
-                char c = exp[i];
-
-                // If the scanned character is an operand, add it to output.  
-                if (char.IsLetterOrDigit(c))
-                {
-                    result += c;
-                }
-
-                // If the scanned character is an '(', push it to the stack.  
-                else if (c == '(')
-                {
-                    stack.Push(c);
-                }
-
-                //  If the scanned character is an ')', pop and output from the stack   
-                // until an '(' is encountered.  
-                else if (c == ')')
-                {
-                    while (stack.Count > 0 && stack.Peek() != '(')
-                    {
-                        result += stack.Pop();
-                    }
-
-                    if (stack.Count > 0 && stack.Peek() != '(')
-                    {
-                        return "Invalid Expression"; // invalid expression 
-                    }
-                    else
-                    {
-                        stack.Pop();
-                    }
-                }
-                else // an operator is encountered 
-                {
-                    while (stack.Count > 0 && Prec(c) <= Prec(stack.Peek()))
-                    {
-                        result += stack.Pop();
-                    }
-                    stack.Push(c);
-                }
-
-            }
-
-            // pop all the operators from the stack  
-            while (stack.Count > 0)
-            {
-                result += stack.Pop();
-            }
-
-            return result;
         }
     }
-
-
-
 }
