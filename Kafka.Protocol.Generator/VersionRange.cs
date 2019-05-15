bd69d4f0e02ea8debe92a42db399dd6ff06a7805
Fredrik Arvidsson <fredrik@fkan.se>
2019-05-15 23:15:31 +0200

    add version range parser

---
+++ Kafka.Protocol.Generator/VersionRange.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Kafka.Protocol.Generator
+{
+    public class VersionRange
+    {
+        private int From { get; }
+        private int To { get; }
+
+        internal VersionRange(int from, int to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public static VersionRange Parse(string versionRangeExpression)
+        {
+            const string expectedFormatMessage = "Expected version range expression. Example: 0+, 1-2";
+
+            if (string.IsNullOrEmpty(versionRangeExpression))
+            {
+                throw new ArgumentNullException(nameof(versionRangeExpression), expectedFormatMessage);
+            }
+
+            var versions = versionRangeExpression.Split(new[] { '-', '+' }, StringSplitOptions.RemoveEmptyEntries);
+            if (versions.Length > 2)
+            {
+                throw new ArgumentException(expectedFormatMessage, nameof(versionRangeExpression));
+            }
+
+            if (int.TryParse(versions.First(), out var from) == false)
+            {
+                throw new ArgumentException($"{versions.First()} is not a number. {expectedFormatMessage}", nameof(versionRangeExpression));
+            }
+
+            var to = int.MaxValue;
+            if (versions.Length == 1)
+            {
+                return new VersionRange(from, to);
+            }
+
+            if (int.TryParse(versions.Last(), out to) == false)
+            {
+                throw new ArgumentException($"{versions.Last()} is not a number. {expectedFormatMessage}", nameof(versionRangeExpression));
+            }
+
+            return new VersionRange(from, to);
+        }
+
+        public bool IsInRange(int version)
+        {
+            return version >= From &&
+                   version <= To;
+        }
+    }
+}
---
+++ Tests/Kafka.Protocol.Generator.Tests/VersionRangeTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Test.It.With.XUnit;
+using Xunit;
+
+namespace Kafka.Protocol.Generator.Tests
+{
+    public partial class Given_an_open_end_range_expression
+    {
+        public class When_parsing : XUnit2Specification
+        {
+            private VersionRange _range;
+
+            protected override void Given()
+            {
+                _range = VersionRange.Parse("0+");
+            }
+
+            [Theory]
+            [InlineData(-1, false)]
+            [InlineData(0, true)]
+            [InlineData(int.MaxValue, true)]
+            public void It_should_create_a_valid_range(int version, bool valid)
+            {
+                _range.IsInRange(version).Should().Be(valid);
+            }
+        }
+    }
+
+    public partial class Given_a_closed_end_range_expression
+    {
+        public class When_parsing : XUnit2Specification
+        {
+            private VersionRange _range;
+
+            protected override void Given()
+            {
+                _range = VersionRange.Parse("1-2");
+            }
+
+            [Theory]
+            [InlineData(0, false)]
+            [InlineData(1, true)]
+            [InlineData(2, true)]
+            [InlineData(int.MaxValue, false)]
+            public void It_should_create_a_valid_range(int version, bool valid)
+            {
+                _range.IsInRange(version).Should().Be(valid);
+            }
+        }
+    }
+}
