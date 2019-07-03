406c9ab31b8e9d21345042bb023e19f7e798a3d2
Fredrik Arvidsson <fredrik@fkan.se>
2019-07-03 22:59:50 +0200

    add record batch timestamp type

---
+++ Kafka.Protocol/Int16Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Kafka.Protocol
+{
+    internal static class Int16Extensions
+    {
+        internal static bool IsBitSet(this Int16 value, int bitNumber)
+        {
+            if (bitNumber < 0 ||
+                bitNumber > 15)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bitNumber), "Must be in range 0-15");
+            }
+
+            if (value.Value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), "Must be equal or greater than 0");
+            }
+
+            return ((int)value.Value)
+                .IsBitSet(bitNumber);
+        }
+
+        internal static int GetValueOfBitRange(this Int16 value, 
+            int fromBit, 
+            int toBit)
+        {
+            if (fromBit < 0 ||
+                fromBit > 15)
+            {
+                throw new ArgumentOutOfRangeException(nameof(fromBit), "Must be in range 0-15");
+            }
+
+            if (toBit < 0 ||
+                toBit > 15)
+            {
+                throw new ArgumentOutOfRangeException(nameof(toBit), "Must be in range 0-15");
+            }
+
+            if (toBit < fromBit)
+            {
+                throw new ArgumentOutOfRangeException(nameof(toBit), $"Must be equal or greater than {nameof(fromBit)}");
+            }
+
+            return ((int) value.Value).GetValueOfBitRange(fromBit, toBit);
+        }
+    }
+}
--- Kafka.Protocol/Records/RecordBatch.cs
+++ Kafka.Protocol/Records/RecordBatch.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Kafka.Protocol.Records
+﻿namespace Kafka.Protocol.Records
 {
     public class RecordBatch : ISerialize
     {
@@ -30,34 +28,38 @@ namespace Kafka.Protocol.Records
                 _recordBatch = recordBatch;
             }
 
-            private enum Attribute
-            {
-                NoCompression = 0,
-                Gzip = 1,
-                Snappy = 2,
-                Lz4 = 3,
-                Zstd = 4,
-            }
-
-            public bool NoCompression =>
-                _recordBatch.Attributes.GetValueOfBitRange(0, 2) ==
-                (int) Attribute.NoCompression;
+            public bool None =>
+                _recordBatch.Attributes.GetValueOfBitRange(0, 2) == 0;
 
             public bool Gzip =>
-                _recordBatch.Attributes.GetValueOfBitRange(0, 2) ==
-                (int) Attribute.Gzip;
+                _recordBatch.Attributes.GetValueOfBitRange(0, 2) == 1;
 
             public bool Snappy =>
-                _recordBatch.Attributes.GetValueOfBitRange(0, 2) ==
-                (int) Attribute.Snappy;
+                _recordBatch.Attributes.GetValueOfBitRange(0, 2) == 2;
 
             public bool Lz4 =>
-                _recordBatch.Attributes.GetValueOfBitRange(0, 2) ==
-                (int) Attribute.Lz4;
+                _recordBatch.Attributes.GetValueOfBitRange(0, 2) == 3;
 
             public bool Zstd =>
-                _recordBatch.Attributes.GetValueOfBitRange(0, 2) ==
-                (int) Attribute.Zstd;
+                _recordBatch.Attributes.GetValueOfBitRange(0, 2) == 4;
+        }
+
+        public TimestampTypes TimestampType => new TimestampTypes(this);
+
+        public class TimestampTypes
+        {
+            private readonly RecordBatch _recordBatch;
+
+            internal TimestampTypes(RecordBatch recordBatch)
+            {
+                _recordBatch = recordBatch;
+            }
+
+            public bool CreateTime =>
+                !LogAppendTime;
+
+            public bool LogAppendTime =>
+                _recordBatch.Attributes.IsBitSet(3);
         }
 
         public void ReadFrom(IKafkaReader reader)
@@ -92,33 +94,6 @@ namespace Kafka.Protocol.Records
             writer.WriteInt16(ProducerEpoch.Value);
             writer.WriteInt32(BaseSequence.Value);
             writer.Write(Records);
-        }
-    }
-
-    internal static class Int16Extensions
-    {
-        internal static bool IsBitSet(this Int16 value, int bitNumber)
-        {
-            if (bitNumber < 0 ||
-                bitNumber > 15)
-            {
-                throw new ArgumentOutOfRangeException(nameof(bitNumber), "Must be in range 0-15");
-            }
-
-            if (value.Value < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(value), "Must be equal or greater than 0");
-            }
-
-            return ((int)value.Value)
-                .IsBitSet(bitNumber);
-        }
-
-        internal static int GetValueOfBitRange(this Int16 value, 
-            int fromBit, 
-            int toBit)
-        {
-            return ((int) value.Value).GetValueOfBitRange(fromBit, toBit);
         }
     }
 }
