10ca65ee767d39d1ba1cc22b33377263f22cd42b
Fredrik Arvidsson <fredrik@fkan.se>
2019-07-09 19:41:18 +0200

    add help methods for handling control batches version and type

--- Kafka.Protocol/Records/ControlRecord.cs
+++ Kafka.Protocol/Records/ControlRecord.cs
@@ -2,9 +2,40 @@
 {
     public class ControlRecord : ISerialize
     {
-        public Int16 Version { get; set; } = Int16.Default;
-        public Int16 Type { get; set; } = Int16.Default;
-        
+        public ControlRecord(Int16 version)
+        {
+            Version = version;
+        }
+
+        private Int16 _version = Int16.Default;
+        public Int16 Version
+        {
+            get => _version;
+            set
+            {
+                if (value != Int16.From(0))
+                {
+                    throw new UnsupportedVersionException($"Version '{value}' is not supported. Supported version is 0");
+                }
+
+                _version = value;
+            }
+        }
+
+        public Int16 Type { get; private set;} = Int16.Default;
+
+        public bool IsAbortMarker
+        {
+            get => Type == Int16.From(0);
+            set => Type = Int16.From(value ? (short)0 : (short)1);
+        }
+
+        public bool IsCommit
+        {
+            get => Type == Int16.From(1);
+            set => Type = Int16.From(value ? (short)1 : (short)0);
+        }
+
         public void ReadFrom(IKafkaReader reader)
         {
             Version = Int16.From(reader.ReadInt16());
