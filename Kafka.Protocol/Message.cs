0c57d309f767278b24603069dcec8e47144871c3
Fredrik Arvidsson <fredrik@fkan.se>
2019-07-22 22:54:30 +0200

    parse protocol messages

---
+++ Kafka.Protocol/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Kafka.Protocol
+{
+    public abstract class Message : ISerialize
+    {
+        public abstract int Version { get; }
+
+        public abstract void ReadFrom(IKafkaReader reader);
+        public abstract void WriteTo(IKafkaWriter writer);
+    }
+}
