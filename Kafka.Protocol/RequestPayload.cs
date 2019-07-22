464d4a210c44d7bdd0258665e7d368791513fa66
Fredrik Arvidsson <fredrik@fkan.se>
2019-07-22 23:37:52 +0200

    add request and response payloads

---
+++ Kafka.Protocol/RequestPayload.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace Kafka.Protocol
+{
+    public sealed class RequestPayload
+    {
+        public RequestHeader Header { get; }
+        public Message Message { get; }
+
+        public RequestPayload(RequestHeader header, Message message)
+        {
+            Header = header;
+            Message = message;
+        }
+
+        internal byte[] WriteTo()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new KafkaWriter(stream))
+                {
+                    Header.WriteTo(writer);
+                    Message.WriteTo(writer);
+                }
+
+                return stream.GetBuffer();
+            }
+        }
+
+        public static RequestPayload ReadFrom(int version, byte[] payload)
+        {
+            var reader = new KafkaReader(payload);
+            var header = new RequestHeader(version);
+            header.ReadFrom(reader);
+            var message = Messages.Create(header.RequestApiKey.Value, version);
+            message.ReadFrom(reader);
+
+            return new RequestPayload(header, message);
+        }
+    }
+}
---
+++ Kafka.Protocol/ResponsePayload.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+namespace Kafka.Protocol
+{
+    public sealed class ResponsePayload
+    {
+        public RequestPayload RequestPayload { get; }
+        public ResponseHeader Header { get; }
+        public Message Message { get; }
+
+        public ResponsePayload(RequestPayload requestPayload, ResponseHeader header, Message message)
+        {
+            RequestPayload = requestPayload;
+            Header = header;
+            Message = message;
+        }
+
+        internal byte[] WriteTo()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new KafkaWriter(stream))
+                {
+                    Header.WriteTo(writer);
+                    Message.WriteTo(writer);
+                }
+
+                return stream.GetBuffer();
+            }
+        }
+
+        public static ResponsePayload ReadFrom(RequestPayload requestPayload, byte[] payload)
+        {
+            var reader = new KafkaReader(payload);
+            var header = new ResponseHeader(requestPayload.Header.Version);
+            header.ReadFrom(reader);
+            var message = Messages.Create(requestPayload.Header.RequestApiKey.Value, requestPayload.Header.Version);
+            message.ReadFrom(reader);
+
+            return new ResponsePayload(requestPayload, header, message);
+        }
+    }
+}
