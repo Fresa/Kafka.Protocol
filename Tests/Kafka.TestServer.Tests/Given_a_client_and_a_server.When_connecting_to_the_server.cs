6aac1841d4dac464a00bcc98147849cbc844ed7c
Fredrik Arvidsson <fredrik@fkan.se>
2020-01-05 16:19:14 -0400

    move base test specification to separate file

--- Tests/Kafka.TestServer.Tests/Given_a_client_and_a_server.When_connecting_to_the_server.cs
+++ Tests/Kafka.TestServer.Tests/Given_a_client_and_a_server.When_connecting_to_the_server.cs
@@ -3,11 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Kafka.Protocol;
-using Log.It;
-using Log.It.With.NLog;
-using Microsoft.Extensions.Configuration;
-using NLog.Extensions.Logging;
-using Test.It.With.XUnit;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -71,31 +66,6 @@ namespace Kafka.TestServer.Tests
                     .ConfigureAwait(false);
                 producer.Flush();
             }
-        }
-    }
-
-    public class TestSpecificationAsync : XUnit2SpecificationAsync
-    {
-        static TestSpecificationAsync()
-        {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .Build();
-
-            NLog.LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
-            LogFactory.Initialize(new NLogFactory(new LogicalThreadContext()));
-        }
-
-        public TestSpecificationAsync(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
-        {
-            NLogCapturingTarget.Subscribe += TestOutputHelper.WriteLine;
-        }
-
-        protected override Task DisposeAsync(bool disposing)
-        {
-            NLogCapturingTarget.Subscribe -= TestOutputHelper.WriteLine;
-            return base.DisposeAsync(disposing);
         }
     }
 }
