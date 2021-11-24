940a5cfe4b6f815cb7c0e740a557a78fe62bcb2f
Fredrik Arvidsson <fredrik@fkan.se>
2021-11-24 22:47:39 +0100

    test(request payload): test that reading an unsupported ApiVersionRequest throws UnsupportedVersion

---
+++ Tests/Kafka.Protocol.Tests/RequestPayloadTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Log.It;
+using Log.It.With.NLog;
+using Test.It.With.XUnit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Kafka.Protocol.Tests
+{
+    public partial class Given_an_unsupported_request_payload
+    {
+        public class When_reading_the_payload : XUnit2SpecificationAsync
+        {
+            private KafkaReader _reader;
+            private Exception _exceptionCaught;
+
+            static When_reading_the_payload()
+            {
+                LogFactory.Initialize(new NLogFactory(new LogicalThreadContext()));
+            }
+
+            protected override async Task GivenAsync()
+            {
+                _reader = await new byte[]
+                    {
+                        0, 0, 0, 10, // Length
+                        0, 18, 0, 100, 0, 0, 0, 0, 0, 0 // Header with ApiRequestVersion api key and unsupported version (100)
+                    }
+                    .ToReaderAsync()
+                    .ConfigureAwait(false);
+            }
+
+            protected override async Task WhenAsync()
+            {
+                try
+                {
+                    await RequestPayload
+                        .ReadFromAsync(RequestHeader.MaxVersion, _reader,
+                            new CancellationTokenSource(TimeSpan.FromSeconds(2))
+                                .Token)
+                        .ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    _exceptionCaught = ex;
+                }
+            }
+
+            [Fact]
+            public void It_should_throw_unsupported_version()
+            {
+                _exceptionCaught.Should().BeOfType<UnsupportedVersionException>();
+            }
+        }
+    }
+}
