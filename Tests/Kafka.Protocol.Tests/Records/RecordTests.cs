c5e79a8a563075bb69890075fffd601bb8bf802e
Fredrik Arvidsson <fredrik@fkan.se>
2021-12-21 19:29:33 +0100

    test(record): assure the length is correctly written

---
+++ Tests/Kafka.Protocol.Tests/Records/RecordTests.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Kafka.Protocol.Records;
+using Test.It.With.XUnit;
+using Xunit;
+using Record = Kafka.Protocol.Records.Record;
+
+namespace Kafka.Protocol.Tests.Records
+{
+    namespace RecordBatchTests
+    {
+        public partial class Given_a_record
+        {
+            public class When_calculating_length : XUnit2SpecificationAsync
+            {
+                private readonly Record _record = new()
+                {
+                    Attributes = 1,
+                    Headers = new []
+                    {
+                        new Header()
+                    },
+                    Key = Encoding.UTF8.GetBytes("key1"),
+                    OffsetDelta = 2,
+                    TimestampDelta = 3,
+                    Value = Encoding.UTF8.GetBytes("value1")
+                };
+
+                private int _actualLength;
+
+                protected override async Task WhenAsync()
+                {
+                    var stream = new MemoryStream();
+                    await using (stream.ConfigureAwait(false))
+                    {
+                        var writer = new KafkaWriter(stream);
+                        await using (writer.ConfigureAwait(false))
+                        {
+                            await _record.WriteToAsync(writer)
+                                .ConfigureAwait(false);
+                        }
+                    }
+
+                    _actualLength = stream.ToArray().Length;
+                }
+
+                [Fact]
+                public void It_should_return_the_length_of_the_record_batch_being_serialized()
+                {
+                    _record.Length.Should()
+                        .BeGreaterThan(0)
+                        .And.Be(_actualLength);
+                }
+            }
+        }
+    }
+}
