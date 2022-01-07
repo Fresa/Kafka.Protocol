467175cafca1b3c79ca00f33769deed34262f736
Fredrik Arvidsson <fredrik@fkan.se>
2022-01-08 00:29:42 +0100

    Revert "add nullable record batch"
    This reverts commit 60a028cab58f169bc54051488c829e7d961de93e.

--- Tests/Kafka.Protocol.Tests/Records/RecordBatchTests.cs
+++ Tests/Kafka.Protocol.Tests/Records/RecordBatchTests.cs
@@ -1,7 +1,8 @@
 ﻿using System.IO;
-using System.Threading;
+using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Kafka.Protocol.Cryptography;
 using Kafka.Protocol.Records;
 using Test.It.With.XUnit;
 using Xunit;
@@ -13,140 +14,53 @@ namespace Kafka.Protocol.Tests.Records
     {
         public partial class Given_a_record_batch
         {
-            private static readonly RecordBatch RecordBatch = new()
+            public class When_calculating_length : XUnit2SpecificationAsync
             {
-                Records = new Array<Record>(),
-                Attributes = 1,
-                BaseOffset = 1,
-                BaseSequence = 2,
-                BatchLength = 3,
-                FirstTimestamp = 5,
-                Magic = 2,
-                LastOffsetDelta = 6,
-                PartitionLeaderEpoch = 7,
-                MaxTimestamp = 8,
-                ProducerEpoch = 9,
-                ProducerId = 10,
-                TimestampType = RecordBatch.Timestamp.CreateTime,
-                Compression = RecordBatch.CompressionType.None,
-                IsControlBatch = false,
-                IsTransactional = true
-            };
-
-            public class When_writing : XUnit2SpecificationAsync
-            {
-                private int _actualLength;
-
-                protected override async Task WhenAsync()
+                private readonly RecordBatch _recordBatch = new()
                 {
-                    var writer = new MemoryStream();
-                    await using (writer.ConfigureAwait(false))
+                    Records = new []
                     {
-                        await RecordBatch.WriteToAsync(writer, false, CancellationToken.None)
-                            .ConfigureAwait(false);
-                    }
-
-                    _actualLength = writer.ToArray().Length;
-                }
-                
-                [Fact]
-                public void It_should_report_correct_size()
-                {
-                    RecordBatch.GetSize(false).Should().Be(_actualLength);
-                }
-            }
-
-            public class When_writing_as_compact : XUnit2SpecificationAsync
-            {
-                
-                private int _actualLength;
-
-                protected override async Task WhenAsync()
-                {
-                    var writer = new MemoryStream();
-                    await using (writer.ConfigureAwait(false))
-                    {
-                        await RecordBatch.WriteToAsync(writer, true, CancellationToken.None)
-                            .ConfigureAwait(false);
-                    }
-
-                    _actualLength = writer.ToArray().Length;
-                }
-
-                [Fact]
-                public void It_should_report_correct_size()
-                {
-                    RecordBatch.GetSize(true).Should().Be(_actualLength);
-                }
-            }
-        }
-
-        public partial class Given_a_nullable_record_batch
-        {
-            private static readonly NullableRecordBatch RecordBatch = new()
-            {
-                Records = new Array<Record>(),
-                Attributes = 1,
-                BaseOffset = 1,
-                BaseSequence = 2,
-                BatchLength = 3,
-                FirstTimestamp = 5,
-                Magic = 2,
-                LastOffsetDelta = 6,
-                PartitionLeaderEpoch = 7,
-                MaxTimestamp = 8,
-                ProducerEpoch = 9,
-                ProducerId = 10,
-                TimestampType = Protocol.Records.RecordBatch.Timestamp.CreateTime,
-                Compression = Protocol.Records.RecordBatch.CompressionType.None,
-                IsControlBatch = false,
-                IsTransactional = true
-            };
-
-            public class When_writing : XUnit2SpecificationAsync
-            {
-                private int _actualLength;
-
-                protected override async Task WhenAsync()
-                {
-                    var writer = new MemoryStream();
-                    await using (writer.ConfigureAwait(false))
-                    {
-                        await RecordBatch.WriteToAsync(writer, false, CancellationToken.None)
-                            .ConfigureAwait(false);
-                    }
-
-                    _actualLength = writer.ToArray().Length;
-                }
-
-                [Fact]
-                public void It_should_report_correct_size()
-                {
-                    RecordBatch.GetSize(false).Should().Be(_actualLength);
-                }
-            }
-
-            public class When_writing_as_compact : XUnit2SpecificationAsync
-            {
+                        new Record()
+                    },
+                    Attributes = 1,
+                    BaseOffset = 1,
+                    BaseSequence = 2,
+                    BatchLength = 3,
+                    FirstTimestamp = 5,
+                    Magic = 2,
+                    LastOffsetDelta = 6,
+                    PartitionLeaderEpoch = 7,
+                    MaxTimestamp = 8,
+                    ProducerEpoch = 9,
+                    ProducerId = 10,
+                    TimestampType = RecordBatch.Timestamp.CreateTime,
+                    Compression = RecordBatch.CompressionType.None,
+                    IsControlBatch = false,
+                    IsTransactional = true
+                };
 
                 private int _actualLength;
 
                 protected override async Task WhenAsync()
                 {
-                    var writer = new MemoryStream();
-                    await using (writer.ConfigureAwait(false))
+                    var stream = new MemoryStream();
+                    await using (stream.ConfigureAwait(false))
                     {
-                        await RecordBatch.WriteToAsync(writer, true, CancellationToken.None)
-                            .ConfigureAwait(false);
+                        var writer = new KafkaWriter(stream);
+                        await using (writer.ConfigureAwait(false))
+                        {
+                            await _recordBatch.WriteToAsync(writer)
+                                .ConfigureAwait(false);
+                        }
                     }
 
-                    _actualLength = writer.ToArray().Length;
+                    _actualLength = stream.ToArray().Length;
                 }
 
                 [Fact]
-                public void It_should_report_correct_size()
+                public void It_should_return_the_length_of_the_record_batch_being_serialized()
                 {
-                    RecordBatch.GetSize(true).Should().Be(_actualLength);
+                    _recordBatch.Length.Should().Be(_actualLength);
                 }
             }
         }
