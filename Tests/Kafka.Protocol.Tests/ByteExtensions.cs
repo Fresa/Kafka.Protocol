188b300a47c742f1e548631e7406877a3d7626be
Fredrik Arvidsson <fredrik@fkan.se>
2022-01-08 00:32:43 +0100

    remove obsolete kafka reader and writer

--- Tests/Kafka.Protocol.Tests/ByteExtensions.cs
+++ Tests/Kafka.Protocol.Tests/ByteExtensions.cs
@@ -6,14 +6,6 @@ namespace Kafka.Protocol.Tests
 {
     internal static class ByteExtensions
     {
-        internal static async Task<KafkaReader> ToKafkaReaderAsync(this byte[] bytes)
-        {
-            var pipe = new Pipe();
-            await pipe.Writer.WriteAsync(new ReadOnlyMemory<byte>(bytes))
-                .ConfigureAwait(false);
-            return new KafkaReader(pipe.Reader);
-        }
-
         internal static async Task<PipeReader> ToReaderAsync(this byte[] bytes)
         {
             var pipe = new Pipe();
