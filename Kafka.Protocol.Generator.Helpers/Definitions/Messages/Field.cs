e25ab881f5ba1ddc918ecd47fa1a61a4ecac5e3a
Fredrik Arvidsson <fresa@fresa.se>
2023-05-13 12:18:39 +0200

    fix(field): field version is no longer required, should fallback on taggedversions if it is missing

--- Kafka.Protocol.Generator.Helpers/Definitions/Messages/Field.cs
+++ Kafka.Protocol.Generator.Helpers/Definitions/Messages/Field.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Kafka.Protocol.Generator.Helpers.Definitions.Messages
 {
@@ -8,7 +9,14 @@ namespace Kafka.Protocol.Generator.Helpers.Definitions.Messages
 
         public string Type { get; set; } = default!;
 
-        public string Versions { get; set; } = default!;
+        private string? _versions;
+        public string Versions
+        {
+            get => (_versions ?? TaggedVersions) ??
+                   throw new NullReferenceException(
+                       $"Both {nameof(Versions)} and {TaggedVersions} are null");
+            set => _versions = value;
+        }
 
         public string? NullableVersions { get; set; }
         
