baef265736ca15bbfa35316e6d5db192027294b7
Fredrik Arvidsson <fresa@fresa.se>
2024-06-29 12:54:36 +0200

    simplify reading text from additional texts providers

---
+++ Kafka.Protocol.SourceGenerator/IncrementalValuesProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Kafka.Protocol.SourceGenerator;
+
+internal static class IncrementalValuesProviderExtensions
+{
+    internal static IncrementalValuesProvider<SourceText> ReadTextFromFilesMatching(
+        this IncrementalValuesProvider<AdditionalText> additionalTextProvider,
+        Regex filePattern) =>
+        additionalTextProvider
+            .Where(file =>
+                filePattern.IsMatch(file.Path))
+            .Select(static (text, token) =>
+                text.GetText(token))
+            .Where(static text => text is not null)
+            .Select(static (text, _) => text!);
+}
--- Kafka.Protocol.SourceGenerator/ProtocolGenerator.cs
+++ Kafka.Protocol.SourceGenerator/ProtocolGenerator.cs
@@ -1,50 +1,40 @@
 ﻿using System.Collections.Immutable;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 using Kafka.Protocol.Generator.Helpers;
 using Kafka.Protocol.Generator.Helpers.Definitions;
 using Kafka.Protocol.Generator.Helpers.Definitions.Messages;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 
-namespace Kafka.Protocol.SourceGenerator
+namespace Kafka.Protocol.SourceGenerator;
+
+[Generator]
+public partial class ProtocolGenerator : IIncrementalGenerator
 {
-    [Generator]
-    public partial class ProtocolGenerator : IIncrementalGenerator
-    {
-        private static readonly Regex SpecificationFileRegex = new(@"^.*(\\|/)MessageDefinitions(\\|/).*.json$");
-        private static readonly Regex ProtocolSpecificationRegex = new(@"^.*(\\|/)ProtocolSpecifications(\\|/)Apache Kafka.html$");
+    private static readonly Regex SpecificationFileRegex = new(@"^.*(\\|/)MessageDefinitions(\\|/).*.json$");
+    private static readonly Regex ProtocolSpecificationRegex = new(@"^.*(\\|/)ProtocolSpecifications(\\|/)Apache Kafka.html$");
 
-        private static readonly JsonSerializerOptions
-            _specificationFileSerializerOptions = new()
-            {
-                ReadCommentHandling = JsonCommentHandling.Skip
-            };
+    private static readonly JsonSerializerOptions
+        SpecificationFileSerializerOptions = new()
+        {
+            ReadCommentHandling = JsonCommentHandling.Skip
+        };
 
     public void Initialize(IncrementalGeneratorInitializationContext context)
-        {
-            //Debugger.Launch();
-            var protocolSpecifications = context.AdditionalTextsProvider
-                .Where(static file =>
-                     ProtocolSpecificationRegex.IsMatch(file.Path))
-                .Select(static (text, token) =>
-                    text.GetText(token))
-                .Where(static text => text is not null)
-                .Select(static (text, _) => text!)
-                .Select(static (text, _) =>
-                {
-                    var apacheKafkaDefinitionPage = new HtmlDocument();
-                    apacheKafkaDefinitionPage.LoadHtml(text.ToString());
-                    return ProtocolSpecification.Load(
-                        apacheKafkaDefinitionPage);
-                })
-                .Collect();
-            var protocolSpecification = protocolSpecifications.Select(
+    {
+        //Debugger.Launch();
+        var protocolSpecification = context.AdditionalTextsProvider
+            .ReadTextFromFilesMatching(ProtocolSpecificationRegex)
+            .Select(static (text, _) =>
+            {
+                var apacheKafkaDefinitionPage = new HtmlDocument();
+                apacheKafkaDefinitionPage.LoadHtml(text.ToString());
+                return ProtocolSpecification.Load(
+                    apacheKafkaDefinitionPage);
+            })
+            .Collect()
+            .Select(
                 static (array, _) =>
                 {
                     if (array.IsEmpty)
@@ -52,55 +42,50 @@ namespace Kafka.Protocol.SourceGenerator
                         throw new InvalidOperationException(
                             $"No protocol specification could be found. Make sure a file matching {ProtocolSpecificationRegex} is added to AdditionalFiles");
                     }
+
                     return array.First();
                 });
-            var primitiveTypes =
-                protocolSpecification.Select(
-                    static (specification, _) =>
-                        specification.PrimitiveTypes.Values);
-            var errorCodes = protocolSpecification.Select(
+        var primitiveTypes =
+            protocolSpecification.Select(
                 static (specification, _) =>
-                    specification.ErrorCodes.Values);
-            var primitiveTypeNames = protocolSpecification
-                .Select(static (specification, _) =>
-                    specification
-                        .PrimitiveTypes.Values
-                        .Select(type =>
-                            type.GetClassName())
-                        .ToArray());
+                    specification.PrimitiveTypes.Values);
+        var errorCodes = protocolSpecification.Select(
+            static (specification, _) =>
+                specification.ErrorCodes.Values);
+        var primitiveTypeNames = protocolSpecification
+            .Select(static (specification, _) =>
+                specification
+                    .PrimitiveTypes.Values
+                    .Select(type =>
+                        type.GetClassName())
+                    .ToArray());
 
-            var specifications = context.AdditionalTextsProvider
-                .Where(static file => SpecificationFileRegex.IsMatch(file.Path));
-            var specificationContent = specifications
-                .Select(static (text, token) =>
-                    text.GetText(token))
-                .Where(static text => text is not null)
-                .Select(static (text, _) => text!);
-            var messageDefinitions = specificationContent
-                .Select(static (text, _) =>
-                    JsonSerializer
-                        .Deserialize<Message>(text.ToString(), _specificationFileSerializerOptions)!
-                        .AddReferencesToFields())
-                .Collect()
-                .Select((array, _) =>
+        var messageDefinitions = context.AdditionalTextsProvider
+            .ReadTextFromFilesMatching(SpecificationFileRegex)
+            .Select(static (text, _) =>
+                JsonSerializer
+                    .Deserialize<Message>(text.ToString(), SpecificationFileSerializerOptions)!
+                    .AddReferencesToFields())
+            .Collect()
+            .Select(static (array, _) =>
+            {
+                if (array.IsEmpty)
                 {
-                    if (array.IsEmpty)
-                    {
-                        throw new InvalidOperationException(
-                            $"No message specification files could be found. Make sure files are added as AdditionalFiles matching {SpecificationFileRegex}");
-                    }
-                    return array;
-                });
+                    throw new InvalidOperationException(
+                        $"No message specification files could be found. Make sure files are added as AdditionalFiles matching {SpecificationFileRegex}");
+                }
+                return array;
+            });
             
-            //context.RegisterSourceOutput(primitiveTypes, GeneratePrimitiveTypes);
-            //context.RegisterSourceOutput(errorCodes, GenerateErrorCodes);
+        //context.RegisterSourceOutput(primitiveTypes, GeneratePrimitiveTypes);
+        //context.RegisterSourceOutput(errorCodes, GenerateErrorCodes);
             
-            context.RegisterSourceOutput(primitiveTypeNames.Combine(messageDefinitions), GenerateMessages);
-        }
+        context.RegisterSourceOutput(primitiveTypeNames.Combine(messageDefinitions), GenerateMessages);
+    }
         
-        private void GenerateErrorCodes(SourceProductionContext arg1, ICollection<ErrorCode> arg2)
-        {
-            arg1.AddSource("test.cs", @"
+    private void GenerateErrorCodes(SourceProductionContext arg1, ICollection<ErrorCode> arg2)
+    {
+        arg1.AddSource("test.cs", @"
 namespace Generated
 {
     public class AdditionalTextList
@@ -111,13 +96,13 @@ namespace Generated
         }
     }
 }");
-        }
+    }
 
-        private void GeneratePrimitiveTypes(SourceProductionContext arg1, ICollection<PrimitiveType> arg2)
+    private void GeneratePrimitiveTypes(SourceProductionContext arg1, ICollection<PrimitiveType> arg2)
+    {
+        foreach (var primitiveType in arg2)
         {
-            foreach (var primitiveType in arg2)
-            {
-                arg1.AddSource($"{primitiveType.Type}.g.cs", $@"
+            arg1.AddSource($"{primitiveType.Type}.g.cs", $@"
 namespace Generated
 {{
     public class {primitiveType.Type}
@@ -128,13 +113,13 @@ namespace Generated
         }}
     }}
 }}");
-            }
-            
         }
+            
+    }
 
-        private void GenerateMessages(SourceProductionContext arg1, (string[] Left, ImmutableArray<Message> Right) arg2)
-        {
-            arg1.AddSource("test3.cs", @"
+    private void GenerateMessages(SourceProductionContext arg1, (string[] Left, ImmutableArray<Message> Right) arg2)
+    {
+        arg1.AddSource("test3.cs", @"
 namespace Generated
 {
     public class AdditionalTextList
@@ -145,8 +130,7 @@ namespace Generated
         }
     }
 }");
-        }
+    }
 
         
-    }
 }
