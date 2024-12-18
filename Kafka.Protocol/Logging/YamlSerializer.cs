94eb5441a400855ed0fa33ea41085a5f103f5a28
Fredrik Arvidsson <fresa@fresa.se>
2024-12-18 22:35:33 +0100

    add simple yaml serializer for logging complex objects

---
+++ Kafka.Protocol/Logging/YamlSerializer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Kafka.Protocol.Logging
+{
+    internal static class YamlSerializer
+    {
+        private const string Empty = "";
+
+        internal static string Serialize(object? obj) =>
+            Serialize(obj, string.Empty, out _).Trim();
+        private static string Serialize(object? obj, string indentation, out bool simple)
+        {
+            simple = true;
+            if (obj is null)
+                return "null";
+            if (TrySerializeSimpleType(obj, out var simpleValue))
+            {
+                return simpleValue;
+            }
+
+            simple = false;
+            var type = obj.GetType();
+            var isMap = type.GetInterfaces().Any(@interface =>
+                (@interface.IsGenericType &&
+                 @interface.GetGenericTypeDefinition() ==
+                 typeof(IDictionary<,>)) ||
+                @interface == typeof(IDictionary)
+            );
+
+            if (obj is IEnumerable list)
+            {
+                return string.Join(Environment.NewLine,
+                    list.Cast<object?>()
+                        .Where(item => item != null)
+                        .Cast<object>().Select(
+                            item => isMap
+                                ? Serialize(item, indentation, out _)
+                                : $"{indentation}- {Serialize(item, indentation + "  ", out _).TrimStart()}"));
+            }
+
+            if (type.IsGenericType && type.GetGenericTypeDefinition() ==
+                typeof(KeyValuePair<,>))
+            {
+                var key = type
+                    .GetProperty("Key")!.GetValue(obj);
+                var value = type
+                    .GetProperty("Value")!.GetValue(obj);
+
+                return $"{indentation}? {Serialize(key, indentation + "  ", out _)}{Environment.NewLine}" +
+                       $"{indentation}: {Serialize(value, indentation + "  ", out _)}";
+            }
+
+            var properties = type
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(info => info.CanRead && info.GetIndexParameters().Length == 0)
+                .ToList();
+
+            return properties.Any()
+                ? string.Join(Environment.NewLine, properties.Select(
+                    property =>
+                    {
+                        var value = property.GetValue(obj, null);
+                        var serializedValue =
+                            Serialize(value, indentation + "  ", out var isSimple);
+                        return
+                            $"{indentation}{property.Name}: {(isSimple ? string.Empty : Environment.NewLine)}{serializedValue}";
+                    }))
+                : string.Empty;
+        }
+
+        private static readonly ConcurrentDictionary<Type, bool> IsSimpleTypeCache = new ConcurrentDictionary<Type, bool>();
+
+        private static bool TrySerializeSimpleType(object obj, [NotNullWhen(true)] out string? serializedValue)
+        {
+            var type = obj.GetType();
+            while (true)
+            {
+                if (IsSimpleTypeCache.GetOrAdd(type, _ =>
+                        type.IsPrimitive ||
+                        type.IsEnum ||
+                        type == typeof(string) ||
+                        type == typeof(decimal) ||
+                        type == typeof(DateTime) ||
+                        type == typeof(DateTimeOffset) ||
+                        type == typeof(TimeSpan) ||
+                        type == typeof(Guid)))
+                {
+                    serializedValue = obj switch
+                    {
+                        bool boolValue => boolValue ? "true" : "false",
+                        decimal value => value.ToString(NumberFormatInfo.InvariantInfo),
+                        double value => value.ToString(NumberFormatInfo.InvariantInfo),
+                        DateTimeOffset value => value.ToString("yyyy-MM-dd'T'HH:mm:ss.fffZ"),
+                        DateTime value => value.ToString("yyyy-MM-dd'T'HH:mm:ss.fffZ"),
+                        TimeSpan value => value.ToString(@"d\.hh\:mm\:ss\.fff"),
+
+                        _ => obj.ToString()
+                    };
+                    return true;
+                }
+
+                type = Nullable.GetUnderlyingType(type);
+                if (type != null)
+                {
+                    continue;
+                }
+
+                serializedValue = null;
+                return false;
+            }
+        }
+    }
+}
---
+++ Tests/Kafka.Protocol.Tests/Logging/YamlSerializerTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Kafka.Protocol.Logging;
+using Test.It.With.XUnit;
+using Xunit;
+
+namespace Kafka.Protocol.Tests.Logging;
+
+public class YamlSerializerTests : XUnit2Specification
+{
+    [Theory]
+    [MemberData(nameof(SimpleValues))]
+    [MemberData(nameof(ComplexObjects))]
+    public void Given_a_simple_object_when_serializing_it_should_be_serialized(object value, string expected)
+    {
+        YamlSerializer.Serialize(value).Should().Be(expected);
+    }
+
+    public static IEnumerable<object?[]> SimpleValues =>
+        new List<object?[]>
+        {
+            new object[] { 1, "1" },
+            new object[] { 1.2, "1.2" },
+            new object[] { (decimal)1.2, "1.2" },
+            new object[] { true, "true" },
+            new object?[] { null, "null" },
+            new object[] { "string", "string" },
+            new object[] { new DateTimeOffset(2024, 2, 11, 8, 42, 54, TimeSpan.Zero), "2024-02-11T08:42:54.000Z" },
+            new object[] { new DateTime(2024, 2, 11, 8, 42, 54), "2024-02-11T08:42:54.000Z" },
+            new object[] { new TimeSpan(11, 8, 42, 54, 123), "11.08:42:54.123" },
+            new object[] { Guid.Parse("1A3B944E-3632-467B-A53A-206305310BAE"), "1a3b944e-3632-467b-a53a-206305310bae" }
+        };
+
+    public static IEnumerable<object?[]> ComplexObjects =>
+        new List<object?[]>
+        {
+            new object[]
+            {
+                new
+                {
+                    Foo = 1, 
+                    Bar = new
+                    {
+                        Foo = "fii"
+                    },
+
+                },
+                """
+                Foo: 1
+                Bar: 
+                  Foo: fii
+                """
+            },
+            new object[]
+            {
+                new
+                {
+                    Foo = new []
+                    {
+                        "bar",
+                    }
+                },
+                """
+                Foo: 
+                  - bar
+                """
+            },
+            new object[]
+            {
+                new[]
+                {
+                    "foo",
+                    "bar"
+                },
+                """
+                - foo
+                - bar
+                """
+            },
+            new object[]
+            {
+                new Dictionary<string, string>
+                {
+                    ["foo"] = "bar",
+                    ["bar"] = "baz"
+                },
+                """
+                ? foo
+                : bar
+                ? bar
+                : baz
+                """
+            },
+            new object[]
+            {
+                new List<KeyValuePair<string, string>>
+                {
+                    new("foo", "bar"),
+                    new("foo", "baz")
+                },
+                """
+                - ? foo
+                  : bar
+                - ? foo
+                  : baz
+                """
+            },
+        };
+}
